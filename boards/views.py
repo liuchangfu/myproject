b14832b1b244690f40793a4bf96892785119561f
Liuchangfu <shift_1220@163.com>
2018-10-21 14:42:22 +0800

    第三次提交

--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.http import Http404
-from .models import Board
+from .models import Board, Topic, Post
 
 
 # Create your views here.
@@ -17,3 +18,28 @@ def board_topics(request, pk):
     #     raise Http404
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'boards/topics.html', locals())
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first()
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+
+        return redirect('board_topics', pk=board.pk)
+
+    return render(request, 'boards/new_topic.html', locals())
