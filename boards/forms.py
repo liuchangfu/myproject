446b14ee7233d2df3c182f624bbcb4817d7b8807
Liuchangfu <shift_1220@163.com>
2018-10-21 18:36:40 +0800

    修改文件

---
+++ boards/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Topic
+
+
+class NewTopicForm(forms.ModelForm):
+    message = forms.CharField(label='内容',
+                              widget=forms.Textarea(attrs={'rows': 5, 'placeholder': '你想输入什么呢'}),
+                              max_length=40000,
+                              help_text='最大长度为4000个字符', )
+
+    class Meta:
+        model = Topic
+        fields = ['subject', 'message']
--- boards/views.py
+++ boards/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.http import Http404
+from .forms import NewTopicForm
 from .models import Board, Topic, Post
 
 
@@ -22,24 +23,20 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
-
-        user = User.objects.first()
-
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
-
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
         return redirect('board_topics', pk=board.pk)
-
+    else:
+        form = NewTopicForm()
     return render(request, 'boards/new_topic.html', locals())
