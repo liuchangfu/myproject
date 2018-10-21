9f09c8c1a8d553a9aa388a3897df4ea4ed63693a
Liuchangfu <shift_1220@163.com>
2018-10-21 12:08:20 +0800

    第一次提交

---
+++ boards/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Board(models.Model):
+    name = models.CharField(max_length=30, unique=True, verbose_name='版块名称')
+    description = models.CharField(max_length=100, verbose_name='描述信息')
+
+    def __str__(self):
+        return self.name
+
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=255, verbose_name='主题')
+    last_updated = models.DateTimeField(auto_now=True, verbose_name='最后更新时间')
+    board = models.ForeignKey(Board, related_name='topics', verbose_name='版块名称')
+    starter = models.ForeignKey(User, related_name='topics', verbose_name='发布者')
+
+
+class Post(models.Model):
+    message = models.TextField(max_length=40000,verbose_name='内容')
+    topic = models.ForeignKey(Topic, related_name='posts',verbose_name='发布主题')
+    created_at = models.DateTimeField(auto_now=True,verbose_name='主题创建时间')
+    updated_at = models.DateTimeField(null=True,verbose_name='主题更新时间')
+    created_by = models.ForeignKey(User, related_name='posts',verbose_name='创建时间')
+    updated_by = models.ForeignKey(User, null=True, related_name='+',verbose_name='更新时间')
