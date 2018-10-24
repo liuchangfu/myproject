ca7744016664aaa6f50b25ff7d0ae1c454dfe9aa
liuchangfu <shift_1220@163.com>
2018-10-24 09:37:20 +0800

    提交.gitignore文件

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # myproject
-Django实践项目
+# Django实践项目
+
+## **环境说明**
+
+1.指定django的版本，安装命令为
+ 
+ pip install django==1.11.4
+
+2.安装django-widget-tweaks模块，安装命令为
+
+pip install django-widget-tweaks模块
+
+3.如果想在虚拟环境下运行，请安装virtualenv模块，安装命令为
+pip install virtualenv
+
+4. 创建虚拟环境步骤
+
+*  在文件夹下直接建立：
+
+先新建文件夹python_ven_demo，
+
+然后进入该文件夹cd python_ven_demo，
+
+然后搭建虚拟环境：python -m venv . （注：venv 之后一个空格加上一点“.”)
+
+* 在桌面上直接建立虚拟环境文件夹 及 虚拟环境：
+
+直接输入 python -m venv python_ven_demo (虚拟环境文件夹名，直接在桌面建立了文件夹，同时生成了虚拟环境）
+
+* 激活虚拟环境[注意：只有激活之后，才算进入该虚拟环境，否则安装包时，依然是安装在全局环境之下]
+
+激活文件在Script文件夹下，激活：activate.bat，退出：deactivate.bat
+
+激活方式为：dos 界面进入python_ven_demo/文件夹，然后输入activate.bat (也可直接输入activate）即可激活环境，输入python 就进入python3.6环境了
+退出方式：输入deactivate.bat 或 deactivate
+
+* 最后进入python虚拟环境，安装所需要的包，安装方式同windows安装方法一样，输入命令pip install  包名
+
+
+
--- boards/models.py
+++ boards/models.py
@@ -15,14 +15,14 @@ class Board(models.Model):
 class Topic(models.Model):
     subject = models.CharField(max_length=255, verbose_name='主题')
     last_updated = models.DateTimeField(auto_now_add=True, verbose_name='最后更新时间')
-    board = models.ForeignKey(Board, related_name='topics', verbose_name='版块名称')
-    starter = models.ForeignKey(User, related_name='topics', verbose_name='发布者')
+    board = models.ForeignKey(Board, related_name='topics', verbose_name='版块名称', on_delete=models.CASCADE)
+    starter = models.ForeignKey(User, related_name='topics', verbose_name='发布者',on_delete=models.CASCADE)
 
 
 class Post(models.Model):
     message = models.TextField(max_length=40000, verbose_name='内容')
-    topic = models.ForeignKey(Topic, related_name='posts', verbose_name='发布主题')
+    topic = models.ForeignKey(Topic, related_name='posts', verbose_name='发布主题', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True, verbose_name='主题创建时间')
     updated_at = models.DateTimeField(null=True, verbose_name='主题更新时间')
-    created_by = models.ForeignKey(User, related_name='posts', verbose_name='创建时间')
-    updated_by = models.ForeignKey(User, null=True, related_name='+', verbose_name='更新时间')
+    created_by = models.ForeignKey(User, related_name='posts', verbose_name='创建时间', on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(User, null=True, related_name='+', verbose_name='更新时间', on_delete=models.CASCADE)
